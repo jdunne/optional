d99d49e1e9cac37015107a5d762b223ab5c8a6bb
John Dunne <johndunne@Johns-MacBook-Air.local>
2017-03-26 12:32:33 -0400

    HasEmpty tests

--- Optional.Tests/Optional_Tests.cs
+++ Optional.Tests/Optional_Tests.cs
@@ -6,9 +6,16 @@ namespace Optional.Tests
     public class Optional_UnitTest
     {
         [Fact]
-        public void Test1()
+        public void TestHasEmpty()
         {
+            var x = Optional.From(123);
+            Assert.True(x.HasValue);
 
+            int? y = null;
+            var x2 = Optional.FromNullable(y);
+            Assert.False(x2.HasValue);
+
+            Assert.False(Optional.Empty<int>().HasValue);
         }
     }
 }
--- Optional/Optional.cs
+++ Optional/Optional.cs
@@ -4,5 +4,40 @@ namespace Optional
 {
     public class Optional
     {
+        static public Optional<T> From<T>(T val)
+        {
+            if(val == null)
+            {
+                throw new NullReferenceException(nameof(val));
+            }
+            return new Optional<T>( val, true );
+        }
+
+        static public Optional<T> FromNullable<T>(T val)
+        {
+            var hasValue = (val != null);
+            return new Optional<T>( val, hasValue );
+        }
+
+        static public Optional<T> Empty<T>()
+        {
+            return new Optional<T>( default(T), false );
+        }
+    }
+
+    public struct Optional<T>
+    {
+        private readonly T _value;
+        private readonly bool _hasValue;
+
+        internal Optional(T value, bool hasValue)
+        {
+            _value = value;
+            _hasValue = hasValue;
+        }
+
+        public bool HasValue => _hasValue;
+
+
     }
 }
